f657a9418445b4e43e1d7c42d6abdb8a5d3e9227
Sambit Basant Naik <sambitbasantnaik@gmail.com>
2024-11-18 02:06:14 +0530

    deployment

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import cookiParser from 'cookie-parser';
+import path from 'path';
 
 import createUser from "./routes/user.route.js";
 import employeeRouter from "./routes/employee.route.js";
@@ -10,6 +11,9 @@ import {connectDB} from './lib/db.js'
 dotenv.config();
 
 const app = express();
+const PORT = process.env.PORT || 5000;
+
+const __dirname = path.resolve();
 
 app.use(express.json({limit:"10mb"}));
 app.use(cookiParser());
@@ -17,7 +21,15 @@ app.use(cookiParser());
 app.use("/api/auth",createUser);
 app.use("/api/employee",employeeRouter);
 
-app.listen(process.env.PORT,()=>{ 
+if (process.env.NODE_ENV === "production") {
+	app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+	app.get("*", (req, res) => {
+		res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+	});
+}
+
+app.listen(PORT,()=>{ 
     console.log("Server is running on port 5000");
     connectDB();
 });
